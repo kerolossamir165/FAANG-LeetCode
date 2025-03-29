8f7fa7098a8ee7300428323119f4b15c51d1fa12
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-03-29 14:11:14 +0200

    Time: 0 ms (100%), Space: 53 MB (60.33%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    let list = []
+    traverse(root, list)
+    return list
+};
+
+function traverse(root, list) {
+    if(root !== null) {
+        traverse(root.left, list)
+        traverse(root.right, list)
+        list.push(root.val)
+    }
+}
