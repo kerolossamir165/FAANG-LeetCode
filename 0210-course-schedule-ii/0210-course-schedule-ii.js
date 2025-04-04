fcc91222ba25c66978dd00b4c30cdc0b9aebffb7
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-04-05 01:19:30 +0200

    Time: 13 ms (46.46%), Space: 64.8 MB (8.13%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -6,47 +6,88 @@
 
 
 var findOrder = function(numCourses, prerequisites) {
-        let adj = {}
+//         let adj = {}
+//     for (let i = 0; i < numCourses; i++) {
+//         adj[i] = []
+//     }
+//     for(let [a , b] of prerequisites) {
+//         adj[b].push(a)
+//     }
+
+//     let stack = []
+//     let hasCycle= false 
+
+//     function dfsOrder(graph) {        
+//         let visited = {};
+//         let recStack = {}
+//          for (let i = 0; i < numCourses; i++) {
+//             if (!visited[i]) {
+//                 if (dfs(graph, visited, i, recStack) === null) {
+//                     hasCycle = true
+//                     return null
+//                 }
+//             }
+//          }
+//     }
+//     function dfs(graph, visited, i, recStack) {
+//         visited[i] = true;
+//         recStack[i] = true
+        
+//         for (let vertex of graph[i]) {
+//             if (!visited[vertex]) {
+//                 if (dfs(graph, visited, vertex, recStack) === null) {
+//                     return null;
+//                 }
+//             } else if (recStack[vertex]) {
+//                 return null;
+//             }
+//         }
+//         delete recStack[i];
+//         stack.push(i);
+//     }
+
+//    dfsOrder(adj)
+    
+//    return hasCycle ? [] : stack.reverse()
+
+    let adj = {}
     for (let i = 0; i < numCourses; i++) {
         adj[i] = []
     }
     for(let [a , b] of prerequisites) {
         adj[b].push(a)
     }
-
+ 
+    let visited = {};
     let stack = []
-    let hasCycle= false 
+    let recStack = {}; 
 
-    function dfsOrder(graph) {        
-        let visited = {};
-        let recStack = {}
-         for (let i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (dfs(graph, visited, i, recStack) === null) {
-                    hasCycle = true
-                    return null
-                }
-            }
-         }
-    }
-    function dfs(graph, visited, i, recStack) {
+   
+    function dfs(graph, visited, i) {
         visited[i] = true;
         recStack[i] = true
-        
-        for (let vertex of graph[i]) {
-            if (!visited[vertex]) {
-                if (dfs(graph, visited, vertex, recStack) === null) {
+
+        for (let ne of graph[i]) {
+            if (!visited[ne]) {
+                if (dfs(graph, visited, ne, recStack) === null) {
                     return null;
                 }
-            } else if (recStack[vertex]) {
+            } else if (recStack[ne]) {
                 return null;
             }
         }
         delete recStack[i];
         stack.push(i);
     }
-
-   dfsOrder(adj)
+    for (let i = 0; i < numCourses; i++) {
+        if (!visited[i]) {
+            if (!visited[i]) {
+                if (dfs(adj, visited, i, recStack) === null) {
+                    return []
+                }
+            }
+        }
+    }
     
-   return hasCycle ? [] : stack.reverse()
+   return stack.reverse()
 };
