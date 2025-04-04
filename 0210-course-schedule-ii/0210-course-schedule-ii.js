4bfba67aa8c8af8c13f7dd9c096a8842eb24b5ff
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-04-05 00:53:06 +0200

    Time: 24 ms (14.58%), Space: 63.7 MB (15.49%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+
+
+var findOrder = function(numCourses, prerequisites) {
+        let adj = {}
+    for (let i = 0; i < numCourses; i++) {
+        adj[i] = []
+    }
+    for(let [a , b] of prerequisites) {
+        adj[b].push(a)
+    }
+
+    let stack = []
+    let hasCycle= false 
+
+    function dfsOrder(graph) {        
+        let visited = {};
+        let recStack = {}
+         for (let i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                if (dfs(graph, visited, i, recStack) === null) {
+                    hasCycle = true
+                    return null
+                }
+            }
+         }
+    }
+    function dfs(graph, visited, i, recStack) {
+        visited[i] = true;
+        recStack[i] = true
+        
+        for (let vertex of graph[i]) {
+            if (!visited[vertex]) {
+                if (dfs(graph, visited, vertex, recStack) === null) {
+                    return null;
+                }
+            } else if (recStack[vertex]) {
+                return null;
+            }
+        }
+        delete recStack[i];
+        stack.push(i);
+    }
+
+   dfsOrder(adj)
+    
+   return hasCycle ? [] : stack.reverse()
+};
