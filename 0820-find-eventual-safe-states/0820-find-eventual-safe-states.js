1d2f9d65d4a0de7e4972c2020d0da3ae8dcd6859
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-04-06 15:00:30 +0200

    Time: 32 ms (45.25%), Space: 65.9 MB (40.66%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[]}
+ */
+var eventualSafeNodes = function(graph) {
+    const visited = {}; 
+    const stack = [];
+    let result = []
+
+    function dfs(vertex) {
+        if (visited[vertex] === 1) {
+            return false
+        }
+        if (visited[vertex] === 2) {
+            return true 
+        }
+
+        visited[vertex] = 1; // Mark as visiting
+        if (graph[vertex]) {
+            for (const neighbor of graph[vertex]) {
+                
+               if(!dfs(neighbor)) return false
+                
+            }
+        }
+        visited[vertex] = 2;
+        stack.push(vertex);
+        return true 
+    }
+
+    for (const vertex in graph) {
+        if(dfs(vertex)) {
+            result.push(parseInt(vertex))
+        }
+        
+    }
+
+    return result
+};
