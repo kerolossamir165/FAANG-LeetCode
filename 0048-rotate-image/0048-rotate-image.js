7982fb8068356622c852232a61c716ed5af2363c
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-07-04 02:45:40 +0300

    Time: 0 ms (100%), Space: 55.3 MB (19.82%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -5,25 +5,39 @@
 var rotate = function(matrix) {
     let n = matrix.length 
 
-    for(let layer = 0 ; layer < Math.floor(n / 2) ; layer++) {
-        let first = layer // start index in the layer
-        let last = n - 1 - layer // end index in the layer
-
-        for(let i = 0 ; i < last - first ; i++ ) {
-            let top = matrix[first][first + i ]
-
-            // Left -to→ Top
-
-            // Bottom -to→ Left
-
-            // Right -to→ Bottom
-
-            // Top (saved) -to→ Right
-
-            matrix[first][first + i] = matrix[last - i ][first]
-            matrix[last - i ][first] = matrix[last][last - i ]
-            matrix[last][last - i ] = matrix[first + i ][last]
-            matrix[first + i][last] = top;
+    for(let i = 0 ; i < n ; i++) {
+        for(let j = i + 1 ; j < n ; j++) {
+            [matrix[i][j],matrix[j][i]] = [matrix[j][i],matrix[i][j]]
         }
     }
+    for(let i = 0 ; i < n ;i++) {
+        matrix[i].reverse()
+    }
 };
+
+
+// var rotate2 = function(matrix) {
+//     let n = matrix.length 
+
+//     for(let layer = 0 ; layer < Math.floor(n / 2) ; layer++) {
+//         let first = layer // start index in the layer
+//         let last = n - 1 - layer // end index in the layer
+
+//         for(let i = 0 ; i < last - first ; i++ ) {
+//             let top = matrix[first][first + i ]
+
+//             // Left -to→ Top
+
+//             // Bottom -to→ Left
+
+//             // Right -to→ Bottom
+
+//             // Top (saved) -to→ Right
+
+//             matrix[first][first + i] = matrix[last - i ][first]
+//             matrix[last - i ][first] = matrix[last][last - i ]
+//             matrix[last][last - i ] = matrix[first + i ][last]
+//             matrix[first + i][last] = top;
+//         }
+//     }
+// };
