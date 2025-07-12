4b2b547f7882aa844a458d0241d9719d8f0c2aed
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-07-12 15:48:41 +0300

    Time: 2 ms (55.57%), Space: 59.1 MB (82.9%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,27 +2,39 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var moveZeroes = function (nums) {
+//     let left = 0
+//     let right = left + 1
+
+//     while (right < nums.length) {
+//         if (nums[left] !== 0) {
+//             left++
+//             right++
+//         }else {
+//             if(nums[right] !== 0) {
+//                 swap(nums, left, right)
+//                 left++
+//             }
+//             right++
+//         }
+//     }
+//     return nums
+// };
+
+// function swap(arr, i, j) {
+//     let temp = arr[i]
+//     arr[i] = arr[j]
+//     arr[j] = temp
+// }
+
 var moveZeroes = function (nums) {
-    let left = 0
-    let right = left + 1
+    let pointer = 0 
 
-    while (right < nums.length) {
-        if (nums[left] !== 0) {
-            left++
-            right++
-        }else {
-            if(nums[right] !== 0) {
-                swap(nums, left, right)
-                left++
-            }
-            right++
+  for(let i = 0 ; i < nums.length ; i++) {
+        if(nums[i] !== 0) {
+            [nums[pointer], nums[i]] = [nums[i], nums[pointer]]
+            pointer++
         }
-    }
-    return nums
-};
-
-function swap(arr, i, j) {
-    let temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
+  }
+  return nums
 }
