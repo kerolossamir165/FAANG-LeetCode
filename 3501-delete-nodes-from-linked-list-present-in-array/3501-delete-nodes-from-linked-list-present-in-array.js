bef1028d95eaf976dbeb902a1054f218bd51f122
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-11-02 02:06:19 +0200

    Time: 80 ms (40.51%), Space: 101.3 MB (30.38%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var modifiedList = function(nums, head) {
+    let dummy = new ListNode() 
+    dummy.next = head  
+    let prev = dummy 
+    let current = head 
+    let values = new Set([...nums])
+   
+    while(current) {
+        if(values.has(current.val)) {
+            prev.next = current.next
+        } else {
+            prev = current
+        }
+        current = current.next
+    }
+    return dummy.next
+    
+};
