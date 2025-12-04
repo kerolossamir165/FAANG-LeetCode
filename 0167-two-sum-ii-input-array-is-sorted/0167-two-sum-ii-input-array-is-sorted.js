d1145dbf12279236c5cc4a5cbddcdd87927dc503
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-12-05 00:01:26 +0200

    Time: 4 ms (18.77%), Space: 55.8 MB (31.58%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,19 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let map = {}
-    for(let i = 0 ; i < numbers.length ; i++) {
-        let sum = target - numbers[i]
-        map[numbers[i]] = i + 1 
-    }    
+     let map = {}
+    
     for (let i = 0; i < numbers.length; i++) {
         
         let sum = target - numbers[i] 
+
         if(map[sum]) {
-            return [i + 1, map[sum]]
+            return [ map[sum], i + 1]
+        } else {
+            map[numbers[i]] = i + 1 
         }
        
     }    
     return []
-    
 };
