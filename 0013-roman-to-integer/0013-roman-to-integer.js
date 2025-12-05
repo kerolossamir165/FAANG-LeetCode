c7c8a36b7fd1e72d6df757ae52e250c3ec1574b7
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-12-06 01:42:59 +0200

    Time: 10 ms (15.55%), Space: 60.8 MB (45.44%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+   const romanValues = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    let sum = 0
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (i - 1 >= 0 && romanValues[s[i]] > romanValues[s[i - 1]]) { 
+            sum += romanValues[s[i]] - romanValues[s[i - 1]]
+            i--
+        } else {
+            sum += romanValues[s[i]]
+        }
+    }
+    return sum
+};
