b67d010fc168eba6fffc4b7852e26dc61dd16547
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-12-15 20:37:34 +0200

    Time: 0 ms (100%), Space: 59.8 MB (46.2%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let destination = nums.length - 1 
+
+    for(let i = nums.length - 1 ; i >= 0 ; i--) {
+        if(i + nums[i] >= destination) {
+            destination = i 
+        }
+    }
+
+    return destination === 0
+    
+};
