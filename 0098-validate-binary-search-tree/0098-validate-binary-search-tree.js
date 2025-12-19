2352806f0149456e61dfb63abf18132a3c74324d
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-12-20 01:23:33 +0200

    Time: 0 ms (100%), Space: 58 MB (74.61%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -22,6 +22,21 @@ var isValidBST = function(root) {
         return check(root.left , min, root.val) && check(root.right, root.val, max)
     }
 
-    return check(root, null , null)
+    // return check(root, null , null)
+    //null is coerced to 0 in numeric comparisons. so we pass infinity
+    return check2(root ,-Infinity, Infinity);
 };
 
+var check2 = function (node , lower , higher) {
+    if(!node) return true 
+
+    if(!(lower < node.val && node.val < higher)) {
+        return false 
+    }
+
+    if(!check2(node.left , lower , node.val)) return false 
+
+    if(!check2(node.right , node.val , higher)) return false
+
+    return true
+}
