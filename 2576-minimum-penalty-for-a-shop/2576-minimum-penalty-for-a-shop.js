df6ac2570b0b36d8b3940fb7a146c95587b8a13d
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2025-12-26 15:12:51 +0200

    Time: 7 ms (93.33%), Space: 59.4 MB (66.67%) - LeetHub

---
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} customers
+ * @return {number}
+ */
+const bestClosingTime = customers => {
+    let bestTime = 0;
+    let minPenalty = 0;
+    let prefix = 0;
+    
+    for (let i = 0; i < customers.length; i++) {
+        prefix += customers[i] === 'Y' ? -1 : 1;
+        
+        if (prefix < minPenalty) {
+            bestTime = i + 1;
+            minPenalty = prefix;
+        }
+    }
+    
+    return bestTime;
+};
