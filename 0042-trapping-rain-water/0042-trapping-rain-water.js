7cb23d7262bcb95ae212120f8a1fdd21553e958a
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2026-01-05 22:41:21 +0200

    Time: 1 ms (66.48%), Space: 56.7 MB (69.72%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let left = 0 , right = height.length - 1 , leftMax = 0 , rightMax = 0 , res = 0 
+
+    while(left < right ) {
+        if(height[left] <= height[right]) {
+            if(height[left] >= leftMax) {
+                leftMax = height[left]
+            } else {
+                res += leftMax - height[left];
+            }
+            left++
+        } else {
+            if(height[right] >= rightMax) {
+                rightMax = height[right]
+            } else {
+                res += rightMax - height[right];
+            }
+            right--  
+        }
+    }
+    return res
+    
+};
