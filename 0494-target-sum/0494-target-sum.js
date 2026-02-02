c573823595666e2ff82e2c52a8422a453f3ec422
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2026-02-02 23:08:37 +0200

    Time: 94 ms (54.15%), Space: 59.6 MB (41.91%) - LeetHub

--- 0494-target-sum/0494-target-sum.js
+++ 0494-target-sum/0494-target-sum.js
@@ -3,22 +3,43 @@
  * @param {number} target
  * @return {number}
  */
-var findTargetSumWays = function(nums, target) {
-    let result = 0 
+var findTargetSumWays = function (nums, target) {
+    // let result = 0 
 
-    function backtracking(index , sum) {
-        if(index === nums.length) {
-            if(sum === target) {
-                result++
-            }
-            return 
+    // function backtracking(index , sum) {
+    //     if(index === nums.length) {
+    //         if(sum === target) {
+    //             result++
+    //         }
+    //         return 
+    //     }
+
+    //     backtracking(index + 1 , sum + nums[index])
+
+    //     backtracking(index + 1 , sum - nums[index])
+
+    // }
+    // backtracking(0 , 0 )
+    // return result 
+
+let memo = new Map();
+
+    function dfs(index, sum) {
+        if (index === nums.length) {
+            return sum === target ? 1 : 0;
         }
 
-        backtracking(index + 1 , sum + nums[index])
+        let key = index + ',' + sum;
+        if (memo.has(key)) return memo.get(key);
 
-        backtracking(index + 1 , sum - nums[index])
+        let add = dfs(index + 1, sum + nums[index]);
+        let subtract = dfs(index + 1, sum - nums[index]);
 
+        memo.set(key, add + subtract);
+        return memo.get(key);
     }
-    backtracking(0 , 0 )
-    return result 
+
+    return dfs(0, 0);
 };
+
+
