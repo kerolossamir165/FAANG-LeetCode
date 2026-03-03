c2cffc12a2c018f43344994a067b961fed1e958a
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2026-03-03 02:03:07 +0200

    Time: 4 ms (40.39%), Space: 61 MB (17.24%) - LeetHub

---
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var largestValues = function (root) {
+    if(!root) return []
+    let result = []
+    let queue = [root]
+
+    while (queue.length) {
+        let level = queue.length
+        let levelMax = -Infinity
+
+        for (let i = 0; i < level; i++) {
+            let node = queue.shift()
+
+            levelMax = Math.max(node.val , levelMax)
+            if (node.left) {
+                queue.push(node.left)
+            }
+
+            if (node.right) {
+                queue.push(node.right)
+            }
+        }
+        result.push(levelMax)
+    }
+    return result
+
+};
