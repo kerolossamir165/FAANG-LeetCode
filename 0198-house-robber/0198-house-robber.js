a2f04f928c014737146cb8defd1f6bffee6b84cf
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2026-04-19 22:33:07 +0200

    Time: 0 ms (100%), Space: 54.2 MB (13.56%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+        if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0];
+
+    let prev1 = 0; // max up to previous house
+    let prev2 = 0; // max up to house before previous
+
+    for (let num of nums) {
+        let temp = prev1;
+        prev1 = Math.max(prev2 + num, prev1);
+        prev2 = temp;
+    }
+
+    return prev1;
+};
