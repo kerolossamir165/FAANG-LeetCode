9ebb4d052e8c3b37f8dd695746d4c4ea5dcc5500
kerolos samir <59576700+kerolossamir165@users.noreply.github.com>
2026-05-11 12:39:14 +0300

    Time: 1 ms (100%), Space: 75.7 MB (62.03%) - LeetHub

---
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+var construct2DArray = function(original, m, n) {
+    if (original.length !== m * n) return [];
+
+    let result = [];
+    for (let i = 0; i < m; i++) {
+        result.push(original.slice(i * n, (i + 1) * n));
+    }
+    return result;
+};
